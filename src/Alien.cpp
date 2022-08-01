e68ba655e8a3e746a4735641a841c6a6ec7d7d71
Max Schrementi <6629474+dwolrdcojp@users.noreply.github.com>
2022-08-01 15:20:01 -0400

    Render a circle

--- src/Alien.cpp
+++ src/Alien.cpp
@@ -1,10 +1,19 @@
 // Compile command
 // g++ --std=c++17 *.cpp -I /opt/homebrew/Cellar/sfml/2.5.1_1 -o prog -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 
 int main()
 {
-  sf::Window window(sf::VideoMode(800, 600), "Alien Shooter");
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Alien Shooter");
+
+  sf::CircleShape circle(100);
+  circle.setFillColor(sf::Color(255, 0, 0));
+  circle.setOutlineThickness(10.0f);
+  circle.setOutlineColor(sf::Color(255, 255, 0));
+  circle.setOrigin(100.f, 100.f);
+  circle.setPosition(sf::Vector2f(window.getSize().x/2.0, window.getSize().y/2.0));
 
   while (window.isOpen())
   {
@@ -13,6 +22,16 @@ int main()
     {
       if (event.type == sf::Event::Closed) { window.close(); }
     }
+
+    // Clear the window with black
+    window.clear(sf::Color::Black);
+
+    // Draw stuff here 
+    window.draw(circle);
+    
+    // End the current frame
+    window.display();
+
   }
 
   return 0;
